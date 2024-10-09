8e4ba95e8dabd6416b36f9cb7d28d6cb467aaec8
AliSaadOthman <96048321+AliSaadOthman@users.noreply.github.com>
2024-10-10 02:27:17 +0300

    removed typescript deployment errors

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  //   output: "export", When deploying uncommit and run npm run build
-  // typescript: {ignoreBuildErrors: true,}
+  output: "export",
+  typescript: { ignoreBuildErrors: true },
 };
 
 export default nextConfig;
