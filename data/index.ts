6182d536bcc1681bddcfc02f34b268898d7cd0b1
AliSaadOthman <96048321+AliSaadOthman@users.noreply.github.com>
2024-10-10 02:18:35 +0300

    initial commit

---
+++ data/index.ts
@@ -0,0 +1,399 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Services", link: "#services" },
+  { name: "Testimonials", link: "#testimonials" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "I prioritize client collaboration, fostering open communication ",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "I'm very flexible with time zone communications",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "I constantly try to improve",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Tech enthusiast with a passion for development.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Currently building a Massively Scalable Microservices App",
+    description: "The Inside Scoop",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Do you want to start a project together?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "Healthcare Patient Management System",
+    des: "allows patients to easily register, book, and manage their appointments with doctors, featuring administrative tools for scheduling, confirming, and canceling appointments, along with SMS notifications.",
+    img: "/CarePulse.svg",
+    iconLists: [
+      "/next.svg",
+      "/tail.svg",
+      "/ts.svg",
+      "/mongo.svg",
+      "/cloudinary.svg",
+    ],
+    link: "https://careplus-five.vercel.app/",
+  },
+  {
+    id: 2,
+    title: "AI Prompt Sharing App",
+    des: "A comprehensive CRUD AI Prompt sharing system utilizing a MongoDB database and implementing NextAuth.",
+    img: "/Promptopia.svg",
+    iconLists: ["/next.svg", "/tail.svg", "/mongo.svg"],
+    link: "https://project-promptopia-mseo.vercel.app/",
+  },
+  {
+    id: 3,
+    title: "XORA - SaaS Landing Page",
+    des: "Xora is a Modern UI/UX SaaS website. With sleek design, seamless animations, and exceptional user experience",
+    img: "/XORA.svg",
+    iconLists: ["/re.svg", "/tail.svg", "/vite.svg"],
+    link: "https://github.com/AliMohamed-SE/XORA-SaaS-Landing-Page",
+  },
+  {
+    id: 4,
+    title: "Animated Apple Iphone 3D Website",
+    des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
+    img: "/p4.svg",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+    link: "https://github.com/adrianhajdin/iphone",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+];
+
+export const companies = [
+  // {
+  //   id: 1,
+  //   name: "cloudinary",
+  //   img: "/cloud.svg",
+  //   nameImg: "/cloudName.svg",
+  // },
+  // {
+  //   id: 2,
+  //   name: "appwrite",
+  //   img: "/app.svg",
+  //   nameImg: "/appName.svg",
+  // },
+  // {
+  //   id: 3,
+  //   name: "HOSTINGER",
+  //   img: "/host.svg",
+  //   nameImg: "/hostName.svg",
+  // },
+  // {
+  //   id: 4,
+  //   name: "stream",
+  //   img: "/s.svg",
+  //   nameImg: "/streamName.svg",
+  // },
+  // {
+  //   id: 5,
+  //   name: "docker.",
+  //   img: "/dock.svg",
+  //   nameImg: "/dockerName.svg",
+  // },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Technical Lead",
+    desc: "Led a team of engineers in the design, development, and deployment of a scalable e-commerce platform. Managed ongoing maintenance and feature upgrades using agile methodologies to ensure continuous improvement.",
+    className: "md:col-span-2",
+    thumbnail: "/exp4.svg",
+  },
+  {
+    id: 2,
+    title: "Software Engineer",
+    desc: "Contributed to the full software development lifecycle of web applications using .NET Core and Blazor. Focused on creating efficient, scalable, and maintainable solutions for enterprise clients.",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 3,
+    title: "AI Solutions Developer",
+    desc: "Developed and integrated AI-driven solutions across multiple client projects, including chatbot systems and large language models to enhance user interaction and automate business processes.",
+    className: "md:col-span-2",
+    thumbnail: "/exp2.svg",
+  },
+  {
+    id: 4,
+    title: "Freelance Web Application Developer",
+    desc: "Spearheaded the development of a custom web application for a client, overseeing the entire project from concept to deployment. Utilized cloud hosting services to ensure optimal performance and scalability.",
+    className: "md:col-span-2",
+    thumbnail: "/exp3.svg",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    title: "github",
+    link: "https://github.com/AliMohamed-SE",
+    img: "/git.svg",
+  },
+  {
+    id: 2,
+    title: "twitter",
+    link: "https://x.com/AliMohamed_Dev",
+    img: "/twit.svg",
+  },
+  {
+    id: 3,
+    title: "linkedin",
+    link: "https://www.linkedin.com/in/ali-saad-othman/",
+    img: "/link.svg",
+  },
+];
+
+export const leftTechStack = ["React.js", "Next.js", "NodeJs"];
+export const rightTechStack = [".Net Core", "Databases", "Laravel"];
+
+export const approachData = [
+  {
+    title: "Planning & Strategy",
+    order: "Phase 1",
+    description:
+      "We'll collaborate to map out your website's goals, target audience, and key functionalities. We'll discuss things like site structure, navigation, and content requirements.",
+    colors: [[153, 255, 187]],
+    dotSize: 2,
+    animationSpeed: 5.1,
+    containerClassName: "bg-black",
+  },
+  {
+    title: "Design & Development",
+    order: "Phase 2",
+    description:
+      "Following our plan, I focus on crafting the visual design and building out the website. I ensure the design aligns with your brand, while implementing functional, clean code.",
+    colors: [[232, 121, 249]],
+    dotSize: 2,
+    animationSpeed: 5.1,
+    containerClassName: "bg-black",
+  },
+  {
+    title: "Testing & Feedback",
+    order: "Phase 3",
+    description:
+      "Before going live, I rigorously test your website to ensure everything works smoothly. You'll have the opportunity to provide feedback, and any necessary adjustments will be made.",
+    colors: [[179, 179, 0]],
+    dotSize: 2,
+    animationSpeed: 5.1,
+    containerClassName: "bg-black",
+  },
+  {
+    title: "Launch & Maintenance",
+    order: "Phase 4",
+    description:
+      "After thorough testing and feedback, your website goes live. Post-launch, I provide ongoing support and updates to ensure your site remains secure and up-to-date.",
+    colors: [[125, 211, 252]],
+    dotSize: 2,
+    animationSpeed: 5.1,
+    containerClassName: "bg-black",
+  },
+];
+
+export const servicesData = [
+  {
+    id: 1,
+    title: "Web Development",
+    description:
+      "Crafting responsive, modern websites using cutting-edge technologies like React, Node.js, and .NET Core.",
+    thumbnail: "globe.svg",
+    features: [
+      "Fully responsive design",
+      "Optimized performance",
+      "SEO-friendly",
+      "Cross-browser compatibility",
+      "Ongoing updates",
+      "Custom-tailored solutions",
+    ],
+  },
+  {
+    id: 2,
+    title: "E-commerce Solutions",
+    description:
+      "Building feature-rich e-commerce platforms with secure payment gateways, inventory management, and user analytics.",
+    thumbnail: "shopping-cart.svg",
+    features: [
+      "Secure payment",
+      "inventory management",
+      "User-friendly admin dashboard",
+      "Analytics and reporting tools",
+      "Customizable checkout process",
+      "Scalable for business growth",
+    ],
+  },
+  {
+    id: 3,
+    title: "Mobile App Development",
+    description:
+      "Developing cross-platform mobile applications with React Native to provide seamless experiences on iOS and Android.",
+    thumbnail: "mobile.svg",
+    features: [
+      "iOS & Android",
+      "Seamless user experience",
+      "Push notifications",
+      "Real-time data synchronization",
+      "Offline mode support",
+      "App store deployment",
+    ],
+  },
+  {
+    id: 4,
+    title: "API Development",
+    description:
+      "Designing and developing robust, scalable RESTful APIs for secure and efficient data communication between systems.",
+    thumbnail: "cogs.svg",
+    features: [
+      "High scalability and performance",
+      "authentication and authorization",
+      "Efficient data processing",
+      "RESTful architecture",
+      "Versioned APIs for future-proofing",
+      "Detailed API documentation",
+    ],
+  },
+  {
+    id: 5,
+    title: "AI Integration",
+    description:
+      "Integrating AI-powered solutions, such as chatbots and machine learning models, to enhance automation and user experiences.",
+    thumbnail: "robot.svg",
+    features: [
+      "Custom chatbot integration",
+      "Predictive analytics and insights",
+      "Machine learning model integration",
+      "Automation of routine tasks",
+      "Seamless user interaction",
+      "Continuous learning",
+    ],
+  },
+  {
+    id: 6,
+    title: "UI/UX Design",
+    description:
+      "Designing intuitive user interfaces and seamless user experiences that are both aesthetically pleasing and functional.",
+    thumbnail: "paint-brush.svg",
+    features: [
+      "User-centered design approach",
+      "Interactive prototypes",
+      "High-fidelity wireframes",
+      "Mobile-first design",
+      "Cross-platform consistency",
+      "Several revisions",
+    ],
+  },
+  {
+    id: 7,
+    title: "SEO Optimization",
+    description:
+      "Optimizing websites to improve search engine ranking and visibility, ensuring your website reaches its target audience.",
+    thumbnail: "search.svg",
+    features: [
+      "Keyword research and optimization",
+      "Technical SEO audits",
+      "Backlink strategy",
+      "Content optimization",
+      "Analytics and reporting",
+      "Performance monitoring",
+    ],
+  },
+  {
+    id: 8,
+    title: "Maintenance & Support",
+    description:
+      "Providing ongoing website maintenance, updates, and technical support to ensure your site remains secure and up-to-date.",
+    thumbnail: "wrench.svg",
+    features: [
+      "24/7 support and monitoring",
+      "Security patches and updates",
+      "Performance optimization",
+      "Regular backups",
+      "Uptime monitoring",
+      "Tailored support packages",
+    ],
+  },
+];
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  //   output: "export", When deploying uncommit and run npm run build
+  // typescript: {ignoreBuildErrors: true,}
+};
 
 export default nextConfig;
