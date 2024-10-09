ed30ab5f4cb75d315240709c6b01362291c036c1
AliSaadOthman <96048321+AliSaadOthman@users.noreply.github.com>
2024-10-10 02:33:16 +0300

    fixed deployment errors

--- app/page.tsx
+++ app/page.tsx
@@ -8,8 +8,6 @@ import RecentProjects from "@/components/RecentProjects";
 import Services from "@/components/Services";
 import { FloatingNav } from "@/components/ui/FloatingNav";
 import { navItems } from "@/data";
-import Image from "next/image";
-import { FaHome } from "react-icons/fa";
 
 export default function Home() {
   return (
